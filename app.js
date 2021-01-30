09da9e28f2b4ee4bb34f73ba1a04e877a222c555
robinsrk <robinsrk3@gmail.com>
2021-01-30 12:37:16 +0600

    added function and cleaned code

--- app.js
+++ app.js
@@ -1,42 +1,79 @@
 // first class plus
-document.getElementById("fc-plus").addEventListener("click", function() {
-    let ticketAmount = parseFloat(document.getElementById("fc-ticket").value);
-    ticketAmount++;
-    document.getElementById("fc-ticket").value = ticketAmount;
-    let ticketPrice = parseFloat(document.getElementById("firstClassPrice").innerText);
-    let subtotal = addAmount("subtotal", ticketPrice);
-    vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
-    // document.
-    // document.getElementById("total").innerText = subtotal + vat;
-    updateAmount("total", subtotal + vat);
-    updateAmount("vat", vat)
-})
+form("fc-plus","fc-minus","fc-ticket","firstClassPrice");
 
-// first class plus
+function form(btnPlus, btnMinus, ticket, price) {
 
-document.getElementById("fc-minus").addEventListener("click", function() {
-    let ticketAmount = parseFloat(document.getElementById("fc-ticket").value);
-    if (ticketAmount >= 1) {
-        ticketAmount--;
-        document.getElementById("fc-ticket").value = ticketAmount;
-        let ticketPrice = parseFloat(document.getElementById("firstClassPrice").innerText);
-        let subtotal = addAmount("subtotal", -1 * ticketPrice);
+    document.getElementById(btnPlus).addEventListener("click", function() {
+        let ticketAmount = parseFloat(document.getElementById(ticket).value);
+        ticketAmount++;
+        document.getElementById(ticket).value = ticketAmount;
+        let ticketPrice = parseFloat(document.getElementById(price).innerText);
+        let subtotal = addAmount("subtotal", ticketPrice);
         vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
         // document.
         // document.getElementById("total").innerText = subtotal + vat;
         updateAmount("total", subtotal + vat);
         updateAmount("vat", vat)
-    }
-    else {
-        alert("Invalid input");
-    }
-})
+    })
+
+    // first class plus
+
+    document.getElementById(btnMinus).addEventListener("click", function() {
+        let ticketAmount = parseFloat(document.getElementById(ticket).value);
+        if (ticketAmount >= 1) {
+            ticketAmount--;
+            document.getElementById(ticket).value = ticketAmount;
+            let ticketPrice = parseFloat(document.getElementById(price).innerText);
+            let subtotal = addAmount("subtotal", -1 * ticketPrice);
+            vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
+            // document.
+            // document.getElementById("total").innerText = subtotal + vat;
+            updateAmount("total", subtotal + vat);
+            updateAmount("vat", vat)
+        }
+        else {
+            alert("Invalid input");
+        }
+    })
+}
+// document.getElementById("fc-plus").addEventListener("click", function() {
+//     let ticketAmount = parseFloat(document.getElementById("fc-ticket").value);
+//     ticketAmount++;
+//     document.getElementById("fc-ticket").value = ticketAmount;
+//     let ticketPrice = parseFloat(document.getElementById("firstClassPrice").innerText);
+//     let subtotal = addAmount("subtotal", ticketPrice);
+//     vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
+//     // document.
+//     // document.getElementById("total").innerText = subtotal + vat;
+//     updateAmount("total", subtotal + vat);
+//     updateAmount("vat", vat)
+// })
+
+// // first class plus
+
+// document.getElementById("fc-minus").addEventListener("click", function() {
+//     let ticketAmount = parseFloat(document.getElementById("fc-ticket").value);
+//     if (ticketAmount >= 1) {
+//         ticketAmount--;
+//         document.getElementById("fc-ticket").value = ticketAmount;
+//         let ticketPrice = parseFloat(document.getElementById("firstClassPrice").innerText);
+//         let subtotal = addAmount("subtotal", -1 * ticketPrice);
+//         vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
+//         // document.
+//         // document.getElementById("total").innerText = subtotal + vat;
+//         updateAmount("total", subtotal + vat);
+//         updateAmount("vat", vat)
+//     }
+//     else {
+//         alert("Invalid input");
+//     }
+// })
 
 // book successful
 
-document.getElementById("book-btn").addEventListener("click", function(){
+document.getElementById("book-btn").addEventListener("click", function() {
     document.getElementById("form").style.display = "none";
-    document.getElementById("success-form").style.display = "block";
+    document.getElementById("success-form").style.display = "flex";
 })
 
 
