0413fc89499b9c3414828d9f02fb1bb7bc71ba41
robinsrk <robinsrk3@gmail.com>
2021-01-30 12:54:32 +0600

    Cleaned code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // first class plus
-form("fc-plus","fc-minus","fc-ticket","firstClassPrice");
-
+form("fc-plus", "fc-minus", "fc-ticket", "firstClassPrice");
+form("eco-plus", "eco-minus", "eco-ticket", "ecoPrice");
 function form(btnPlus, btnMinus, ticket, price) {
 
     document.getElementById(btnPlus).addEventListener("click", function() {
@@ -36,47 +36,19 @@ function form(btnPlus, btnMinus, ticket, price) {
         }
     })
 }
-// document.getElementById("fc-plus").addEventListener("click", function() {
-//     let ticketAmount = parseFloat(document.getElementById("fc-ticket").value);
-//     ticketAmount++;
-//     document.getElementById("fc-ticket").value = ticketAmount;
-//     let ticketPrice = parseFloat(document.getElementById("firstClassPrice").innerText);
-//     let subtotal = addAmount("subtotal", ticketPrice);
-//     vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
-//     // document.
-//     // document.getElementById("total").innerText = subtotal + vat;
-//     updateAmount("total", subtotal + vat);
-//     updateAmount("vat", vat)
-// })
-
-// // first class plus
-
-// document.getElementById("fc-minus").addEventListener("click", function() {
-//     let ticketAmount = parseFloat(document.getElementById("fc-ticket").value);
-//     if (ticketAmount >= 1) {
-//         ticketAmount--;
-//         document.getElementById("fc-ticket").value = ticketAmount;
-//         let ticketPrice = parseFloat(document.getElementById("firstClassPrice").innerText);
-//         let subtotal = addAmount("subtotal", -1 * ticketPrice);
-//         vat = (subtotal * parseFloat(document.getElementById("vatPercentage").innerText)) / 100;
-//         // document.
-//         // document.getElementById("total").innerText = subtotal + vat;
-//         updateAmount("total", subtotal + vat);
-//         updateAmount("vat", vat)
-//     }
-//     else {
-//         alert("Invalid input");
-//     }
-// })
-
 // book successful
+hideUnhide("book-btn", "form", "success-form");
+hideUnhide("home", "success-form", "form");
 
-document.getElementById("book-btn").addEventListener("click", function() {
-    document.getElementById("form").style.display = "none";
-    document.getElementById("success-form").style.display = "flex";
-})
+// go home
 
+function hideUnhide(btn, hide, unhide) {
 
+    document.getElementById(btn).addEventListener("click", function() {
+        document.getElementById(hide).style.display = "none";
+        document.getElementById(unhide).style.display = "block";
+    })
+}
 function addAmount(id, amount) {
     const current = parseFloat(document.getElementById(id).innerText);
     const total = current + amount;
